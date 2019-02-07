f10a24d3571ad9b6992c031ecd66650141fa1819
John Fake <jwfakejr@gmail.com>
2019-02-07 15:41:32 -0600

    final changes...1..2

---
+++ src/app/core/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {Ingredient} from "../../shared/ingredient.model";
+import {Recipe} from "../../recipes/recipe.model";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  //private recipes: Recipe[] = [ ];
+  //   new Recipe(
+  //     'Tasty Schnitzel',
+  //     'A super-tasty Schnitzel - just awesome!',
+  //     'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]),
+  //   new Recipe('Big Fat Burger',
+  //     'What else you need to say?',
+  //     'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+  //     [
+  //       new Ingredient('Buns', 2),
+  //       new Ingredient('Meat', 1)
+  //     ])
+  // ];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
